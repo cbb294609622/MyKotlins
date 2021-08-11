b1740850133089a320240e4ac795e5c1ad017307
cbb <chenbobo@aibabel.com>
2021-08-11 15:23:24 +0800

    使用livedata观察数据变化进而修改布局参数

--- 09_unscramble/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ 09_unscramble/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -61,19 +61,27 @@ class GameFragment : Fragment() {
         binding.submit.setOnClickListener { onSubmitWord() }
         binding.skip.setOnClickListener { onSkipWord() }
         // Update the UI
-        updateNextWordOnScreen()
         binding.score.text = getString(R.string.score, 0)
         binding.wordCount.text = getString(
                 R.string.word_count, 0, MAX_NO_OF_WORDS)
+        viewModel.currentScrambledWord.observe(viewLifecycleOwner,{
+            Log.e("GameFragment", "观察到数据改变：$it")
+            binding.textViewUnscrambledWord.text = it
+        })
+        viewModel.score.observe(viewLifecycleOwner,{
+            binding.score.text = getString(R.string.score,it)
+        })
+        viewModel.currentWordCount.observe(viewLifecycleOwner,{
+            binding.wordCount.text = getString(R.string.word_count,it, MAX_NO_OF_WORDS)
+        })
     }
 
     private fun onSubmitWord(){
         val playerWord = binding.textInputEditText.text.toString()
         if (viewModel.isUserWordCorrect(playerWord)){
             setErrorTextField(false)
-            if (viewModel.nextWord())
-                updateNextWordOnScreen()
-            else showFinalScoreDialog()
+            if (!viewModel.nextWord())
+                showFinalScoreDialog()
         }else{
             setErrorTextField(true)
         }
@@ -82,7 +90,6 @@ class GameFragment : Fragment() {
     private fun onSkipWord(){
         if (viewModel.nextWord()){
             setErrorTextField(false)
-            updateNextWordOnScreen()
         }else{
             showFinalScoreDialog()
         }
@@ -104,7 +111,6 @@ class GameFragment : Fragment() {
     private fun restartGame() {
         viewModel.reinitializeData()
         setErrorTextField(false)
-        updateNextWordOnScreen()
     }
 
     /*
@@ -127,13 +133,6 @@ class GameFragment : Fragment() {
         }
     }
 
-    /*
-     * Displays the next scrambled word on screen.
-     */
-    private fun updateNextWordOnScreen() {
-        binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
-    }
-
     override fun onDetach() {
         super.onDetach()
         Log.e("GameFragment", "GameFragment destroyed!" )
@@ -142,7 +141,7 @@ class GameFragment : Fragment() {
     private fun showFinalScoreDialog(){
         MaterialAlertDialogBuilder(requireContext())
             .setTitle(getString(R.string.congratulations))
-            .setMessage(getString(R.string.you_scored,viewModel.score))
+            .setMessage(getString(R.string.you_scored,viewModel.score.value))
             .setCancelable(false)
             .setNegativeButton(getString(R.string.exit)){_,_ ->
                 exitGame()
